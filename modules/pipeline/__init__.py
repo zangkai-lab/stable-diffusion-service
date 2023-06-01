15c32671e90d72c21d1ab031fbce9df5a6920eef
zangkai-lab <zang__kai@163.com>
2023-06-01 19:58:50 +0800

    delete modules

--- modules/pipeline/__init__.py
+++
@@ -1,3 +0,0 @@
-from .core import *
-from .inference import *
-from .train import *
--- modules/pipeline/mixins/__init__.py
+++
@@ -1,6 +0,0 @@
-from .withRegister import *
-from .iblock import *
-from .idata import *
-from .serializable import *
-from .config import *
-from .itrainer import *
